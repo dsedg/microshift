c7ea96c323a787cd08f95db4c17f80c6f67266fb
dsedg <david.sedgmen@gmail.com>
2025-01-02 00:32:48 +1000

    Update install_go.sh

--- okd/src/install_go.sh
+++ okd/src/install_go.sh
@@ -6,11 +6,11 @@ if [ ! -d "${GO_INSTALL_DIR}" ]; then
     # This is installed into different location (/usr/local/bin/go) from dnf installed Go (/usr/bin/go) so it doesn't conflict
     # /usr/local/bin is before /usr/bin in $PATH so newer one is picked up
     curl -L -o "go${GO_VER}.linux-${GO_ARCH}.tar.gz" "https://go.dev/dl/go${GO_VER}.linux-${GO_ARCH}.tar.gz"
-    sudo rm -rf "/usr/local/go${GO_VER}"
-    sudo mkdir -p "/usr/local/go${GO_VER}"
-    sudo tar -C "/usr/local/go${GO_VER}" -xzf "go${GO_VER}.linux-${GO_ARCH}.tar.gz" --strip-components 1
-    sudo rm -rfv /usr/local/bin/{go,gofmt}
-    sudo ln --symbolic /usr/local/go${GO_VER}/bin/{go,gofmt} /usr/local/bin/
+    rm -rf "/usr/local/go${GO_VER}"
+    mkdir -p "/usr/local/go${GO_VER}"
+    tar -C "/usr/local/go${GO_VER}" -xzf "go${GO_VER}.linux-${GO_ARCH}.tar.gz" --strip-components 1
+    rm -rfv /usr/local/bin/{go,gofmt}
+    ln --symbolic /usr/local/go${GO_VER}/bin/{go,gofmt} /usr/local/bin/
     rm -rfv "go${GO_VER}.linux-${GO_ARCH}.tar.gz"
 fi
 
